e1ab1f66985b157fd9b55cb2bbbf824d1b393e26
JaviCeRodriguez <javicerodriguez@gmail.com>
2025-02-03 15:51:46 -0300

    add: MapList component in initial page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useIsMobile } from "../hooks/useIsMobile";
-
-import CommunityList from "../components/CommunityList";
-import ArgentinaMap from "../components/ArgentinaMap";
-import AddCommunityButton from "../components/AddCommunityButton";
+import MapList from "../components/MapList";
 
 import { fetchCommunities } from "../utils/fetchCommunities";
 import type { Community } from "@/types/community";
@@ -15,7 +11,6 @@ export default function Home() {
   const [hoveredCommunityId, setHoveredCommunityId] = useState<string | null>(
     null
   );
-  const isMobile = useIsMobile();
 
   useEffect(() => {
     const loadCommunities = async () => {
@@ -26,37 +21,20 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <div className="text-center mb-12">
-        <h1 className="text-4xl font-bold mb-4 tech-gradient">
+    <div className="container px-4 py-8 mx-auto">
+      <div className="mb-12 text-center">
+        <h1 className="mb-4 text-4xl font-bold tech-gradient">
           Tech Communities in Argentina
         </h1>
         <p className="text-xl text-muted-foreground">
           Discover and connect with tech enthusiasts across the country
         </p>
       </div>
-      <div className="flex flex-col lg:flex-row gap-8">
-        {!isMobile && (
-          <div className={isMobile ? "w-full" : "lg:w-2/3"} style={{height:800}}>
-            <ArgentinaMap
-              communities={communities}
-              onHoverCommunity={setHoveredCommunityId}
-            />
-          </div>
-        )}
-        <div className={isMobile ? "w-full mt-8" : "lg:w-1/3"}>
-          <div className="bg-card rounded-lg shadow-lg p-6">
-            <div className="flex justify-between items-center mb-6">
-              <h2 className="text-2xl font-semibold">Communities</h2>
-              <AddCommunityButton />
-            </div>
-            <CommunityList
-              communities={communities}
-              hoveredCommunityId={hoveredCommunityId}
-            />
-          </div>
-        </div>
-      </div>
+      <MapList
+        communities={communities}
+        hoveredCommunityId={hoveredCommunityId}
+        onHoverCommunity={setHoveredCommunityId}
+      />
     </div>
   );
 }
